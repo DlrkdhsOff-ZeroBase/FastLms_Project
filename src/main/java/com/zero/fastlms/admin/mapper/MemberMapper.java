db19bff5889d7f1327590e6276346f25e4289b65
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-06 01:20:13 +0900

    관리자 회원 검색 기능

--- src/main/java/com/zero/fastlms/admin/mapper/MemberMapper.java
+++ src/main/java/com/zero/fastlms/admin/mapper/MemberMapper.java
@@ -1,11 +1,12 @@
 package com.zero.fastlms.admin.mapper;
 
 import com.zero.fastlms.admin.dto.MemberDto;
+import com.zero.fastlms.admin.model.MemberParam;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface MemberMapper {
-    List<MemberDto> selectList(MemberDto parameter);
+    List<MemberDto> selectList(MemberParam parameter);
 }
